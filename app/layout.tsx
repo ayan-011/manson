672ba64d3d117b1e15677274c1103cb9622eaf4f
ayan-011 <ayansaifi2nd@gmail.com>
2026-08-11 12:51:50 +0530

    add text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const Might = localFont({
+  src: "../public/font/might/Might.ttf",
+  variable: "--font-might",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col ${Might.variable}`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,20 @@ const Page: React.FC = () => {
           alt=""
           className="w-full h-screen object-cover overflow-hidden opacity-80  "
         />
+        <div className="absolute flex gap-2 p-5 left-0 top-0 bg- itemscenter">
+
+          <h1 className='  left-0 top-0 text-3xl  flex items-center tracking- font-bold might text-white/80'>MISTRI</h1>
         <img
   src="/greek.gif"
   alt=""
-  className="w-92 absolute left-32 top-32 opacity-90"
+  className="w-10 -mt-1    left-2 top-2 opacity-90"
 />
+        </div>
+<h1 className='absolute left-32 top-48  font-bold might text-red-600 flex flex-col '>
+  <span className='text-[15vw] tracking-tighter'>8 <span className="text-white/80  ">to</span> 6</span>
+  <span className='text-[8.5vw]  text-white/80 -mt-32'>NONSTOP</span>
+     
+</h1>
         <YouTubePlayer />
         <Playlist />
       </div>
