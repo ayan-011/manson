10ce82db86ed8f7cef9314fe8e254315db811548
ayan-011 <ayansaifi2nd@gmail.com>
2026-08-13 11:18:39 +0530

    improve logo

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Page: React.FC = () => {
           alt=""
           className="w-full h-screen object-cover overflow-hidden opacity-80  "
         />
-        <div className="absolute flex gap-2 p-5 left-0 top-0 bg- itemscenter">
+        <div className="absolute flex gap-2 p-5 left-3 top-0 bg- itemscenter">
 
           <h1 className='  left-0 top-0 text-3xl  flex items-center tracking- font-bold might text-white/80'>MISTRI</h1>
         <img
@@ -21,7 +21,7 @@ const Page: React.FC = () => {
   className="w-10 -mt-1    left-2 top-2 opacity-90"
 />
         </div>
-<h1 className='might absolute w-full px-4 text-center left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:w-auto lg:px-0 lg:text-left lg:left-32 lg:top-48 lg:translate-x-20 lg:translate-y-0 font-bold text-red-600 flex flex-col'>
+<h1 className=' might absolute w-full px-4 text-center left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:w-auto lg:px-0 lg:text-left lg:left-32 lg:top-48 lg:translate-x-20 lg:translate-y-0 font-bold text-red-600 flex flex-col'>
   <span className='text-[30vw] sm:text-[32vw] lg:text-[17vw] lg:tracking-tighter tracking-tight leading-none'>
     8 <span className="text-white/80">to</span> 6
   </span>
